43774adaa07084be30af7f0c220779ba57e8a3b0
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-09-15 13:34:03 -0400

    Improved the barebones movement script
    - Improved the movement script by using Unity's baked-in movement controls.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,36 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space")) {
-            GetComponent<Rigidbody>().velocity = new Vector3(0,3,0);
-        }
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKey("up")) {
-            GetComponent<Rigidbody>().velocity = new Vector3(0,0,-3);
-        }
+        rb.velocity = new Vector3(horizontalInput * -5f, rb.velocity.y, verticalInput * -5f);
 
-        if (Input.GetKey("down"))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 3);
-        }
-
-        if (Input.GetKey("right"))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(-3, 0, 0);
-        }
-
-        if (Input.GetKey("left"))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(3, 0, 0);
+        if (Input.GetButtonDown("Jump")) {
+            rb.velocity = new Vector3(rb.velocity.x, 3f, rb.velocity.z);
         }
     }
 }
