ec96d337233d645760859327876fe3a4ca4f881a
epicjakman <epicjakman@gmail.com>
2023-09-20 16:04:29 -0400

    Better Jumping, Goofling, Prefabs, Open Spline Rails, general physics improvements

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,14 @@
 using UnityEngine;
 using UnityEngine.Splines;
 using Unity.Mathematics;
-using System.Timers;
-using Unity.Burst.Intrinsics;
-using Unity.VisualScripting;
-using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     // stored objects
     private SplineContainer spline;
     private Collider splineCollider;
     public Transform cameraObj;
-
+    public Transform model;
+    public ParticleSystem tempFlingParticle;
     //deprecated
     public Vector3 playerVel = Vector3.zero;
     private float storedSign = 0;
@@ -20,16 +17,31 @@ public class Player : MonoBehaviour
     //Inputs
     private Vector2 inputVector = Vector2.zero;
     private bool jumpInput = false;
+    private bool gooflingInput = false;
     //rigid body
     public ConfigurableJoint joint;
     public Rigidbody rb;
     
+    //this will be used whenever the player is in the air
+    private GameObject fakeObject;
+    private Rigidbody fakeRB;
+    private ConfigurableJoint fakeJoint;
+    [SerializeField]
+    private GameObject fakePrefab;
+    
+    private float gooflingCharge = 0;
+    //time before the goofling starts to decay
+    private float gooflingResetTimer = 0;
+    public float gooflingMultiplier = 5;
+
     public bool OnRail = true;
+    public bool BelowRail = false;
+    //HOPEFULLY prevent reattatching to the rail
+    private float jumpRegroundCooldown = .2f;
     void Start()
     {
         if(!rb)
             rb = GetComponent<Rigidbody>();
-        rb.AddForce(transform.forward * 10,ForceMode.Impulse);
         //get the camera object pls :)
         cameraObj = Camera.main.transform;
         //find the nearest spline object for a rail
@@ -75,7 +87,8 @@ public class Player : MonoBehaviour
         inputVector.x = Input.GetAxis("Horizontal");
         inputVector.y = Input.GetAxis("Vertical");
         inputVector = inputVector.normalized;
-        jumpInput = Input.GetButton("Jump");
+        jumpInput = Input.GetButtonDown("Jump");
+        gooflingInput = Input.GetButtonDown("Fire1");
     }
 
     private void DisableJoint()
@@ -107,103 +120,233 @@ public class Player : MonoBehaviour
     }
     // Update is called once per frame
     Vector3 lastNearestPoint =Vector3.zero;
+    private bool toggledBelowRail = false;
+    private void Update() {
+        HandleInput();
+        //polish this pls :)
+        Debug.DrawRay(transform.position,jumpUpVector * 3,Color.blue);
+        if(jumpInput && OnRail && !BelowRail)
+        {
+            splineCollider.enabled = false;
+            HandleJump(jumpUpVector,2);
+            //HandleJump(Vector3.up,1);
+        }
+        if(gooflingInput && OnRail && !BelowRail && gooflingCharge >0)
+        {
+            splineCollider.enabled = false;
+            HandleJump(jumpUpVector,Mathf.Clamp(gooflingMultiplier * gooflingCharge,2,5));
+            //HandleJump(Vector3.up,1);
+            tempFlingParticle.Play();
+        }
+    }
+    private Vector3 jumpUpVector = Vector3.up;
     void FixedUpdate()
     {
-        Vector3 oldPos = transform.position;
-        float time = 0;
-        float3 point = 0;
-        
+        //I intended to split it into multiple functions
+        //however, its a bit too complex rn, so for the sake of cleaning it up its all one function 
+        //HandleInput();
+        toggledBelowRail = false;
+        if(inputVector.y < 0)
+        {
+            if(!BelowRail)
+            toggledBelowRail = true;
+            BelowRail = true;
+        }
+        if(inputVector.y > 0)
+        {
+            BelowRail = false;
+        }
+
+
+        MainState();
+    }
+
+    private float debugTime = 0;
+    private void MainState()
+    {
         //we can probably cache this result until the player jumps or touches a new rail
         //for now i am lazy
+        
         using var native = new NativeSpline(spline.Spline, spline.transform.localToWorldMatrix);
-        SplineUtility.GetNearestPoint(native, transform.position,out point,out time);
+        SplineUtility.GetNearestPoint(native, transform.position,out float3 point,out float time);
 
         Vector3 upVector = spline.EvaluateUpVector(time);
         upVector = upVector.normalized;
         Vector3 tangentVector = spline.EvaluateTangent(time);
         tangentVector = tangentVector.normalized;
-        HandleInput();
+        //time = Mathf.Clamp(time,0,1);
+       //HandleInput();
         
         //get player input relative to the camera
-        Vector3 VelocityChange = cameraObj.forward * inputVector.y + cameraObj.right * inputVector.x;
+        Vector3 VelocityChange = cameraObj.right * inputVector.x;
         //prevent the player from being able to affect the y velocity
         VelocityChange.z = VelocityChange.y;
         VelocityChange.y = 0;
         lastNearestPoint = (Vector3) point;
-        if(jumpInput && OnRail)
-        {
-            PartiallyDisableJoint();
-            rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
-            transform.position += Vector3.up * .5f;
-            OnRail = false;
-        }
-        //this is both axeese added together essentially. i think
-        //actually idk if this is even used anymore lmao
-        Vector3 funkyAxis = Vector3.Cross(tangentVector,upVector);
+        jumpUpVector = upVector;
         
-        //the force for playermovement
-        rb.AddForce(VelocityChange * 10,ForceMode.Acceleration);
-        
-        //move the anchors of the join to allow the rail physics to work correctly
-        //should be somewhat disabled when jumping
-        joint.connectedAnchor = (Vector3)point;//spline.transform.TransformPoint((Vector3)point);
         if(!OnRail)
         {
+            if(jumpRegroundCooldown >0)
+                jumpRegroundCooldown -= Time.fixedDeltaTime;
+            gooflingCharge = 0;
+            gooflingResetTimer = 0;
+            //transform.rotation = Quaternion.LookRotation(rb.velocity,Vector3.up);
+            //reset the model's offset when in the air
+            model.transform.localPosition = Vector3.zero;
             splineCollider.enabled = true;
-            RaycastHit hit;
+            BelowRail = false;
             PartiallyDisableJoint();
-            //splines should be given their own layer!!!
-           
-            //get the nearest postiion to the hit, this is a lot more precise than getting the closest
-            //position directly relative to the player
-            if(Physics.SphereCast(transform.position, 1,Vector3.down,out hit))
+            //create the fake rigidbody object
+            if(!fakeObject)
             {
-                SplineUtility.GetNearestPoint(native, hit.point,out point,out time);
-                Vector3 fromPoint = (Vector3)point;
-                lastNearestPoint = (Vector3)point;
-                var newAnchor = new Vector3(point.x,transform.position.y,point.z);
-                joint.connectedAnchor = spline.transform.TransformPoint(newAnchor);
-                //recalculate the vectors
-                upVector = spline.EvaluateUpVector(time);
-                upVector = upVector.normalized;
-                tangentVector = spline.EvaluateTangent(time);
-                tangentVector = tangentVector.normalized;
+                fakeObject = Instantiate(fakePrefab,transform.position,transform.rotation);
+                fakeObject.transform.parent = null;
+                fakeRB = fakeObject.AddComponent<Rigidbody>();
+                fakeJoint = fakeObject.AddComponent<ConfigurableJoint>();
+                fakeJoint.xMotion = ConfigurableJointMotion.Free;
+                fakeJoint.yMotion = ConfigurableJointMotion.Locked;
+                fakeJoint.zMotion = ConfigurableJointMotion.Locked;
+                fakeJoint.angularXMotion = ConfigurableJointMotion.Locked;
+                fakeJoint.angularYMotion = ConfigurableJointMotion.Locked;
+                fakeJoint.angularZMotion = ConfigurableJointMotion.Locked;
+                fakeJoint.autoConfigureConnectedAnchor = false;
             }
-            else
-            {
-                Debug.Log("Misalined from rail? thats fucked up.");
-                var newAnchor = new Vector3(lastNearestPoint.x,transform.position.y,lastNearestPoint.z);
-                joint.connectedAnchor = spline.transform.TransformPoint(newAnchor);
-                //this is to make sure you don't go past the rail through fucky bullshit that happens
-                if(transform.position.y < lastNearestPoint.y)
-                {
-                    OnRail = true;
 
-                }
-                //DisableJoint();
-            }
+            //keep the velocity of the fake rigidbody the same, always
+            fakeRB.velocity = rb.velocity;
+            fakeRB.MovePosition(new Vector3(transform.position.x,fakeObject.transform.position.y,transform.position.z));
+            SplineUtility.GetNearestPoint(native, fakeObject.transform.position,out point,out time);
+            //time = Mathf.Clamp(time,0,1);
+            upVector = spline.EvaluateUpVector(time);
+            upVector = upVector.normalized;
+            tangentVector = spline.EvaluateTangent(time);
+            tangentVector = tangentVector.normalized;
+            
+
+            fakeJoint.connectedAnchor = (Vector3)point;
+            fakeJoint.axis = tangentVector;
+
+            joint.connectedAnchor = transform.position;
+            joint.axis = tangentVector;
+            
+            
+            //FindNewRailCast();
+        
         }
         else
         {
+            jumpRegroundCooldown = .15f;
+            if(fakeObject)
+                DestroyImmediate(fakeObject);
             //renable the join when you ARE on a rail
             EnableJoint();
+            //move the model's position based on if we are hanging on the rail or not
+            model.transform.localPosition = BelowRail?-upVector * .5f:upVector * .5f;
+
+            joint.connectedAnchor = (Vector3)point;
+            joint.axis = tangentVector;
+           
+
+            var prevCharge = gooflingCharge;
+
+            //build charge when below the rail
+            if(BelowRail){
+                gooflingCharge += Time.fixedDeltaTime ;
+                gooflingResetTimer = 2;
+                }
+
+            if(gooflingResetTimer > 0)
+                gooflingResetTimer -= Time.fixedDeltaTime;
+
+
+
+            if(gooflingResetTimer <= 0 && gooflingCharge >= 0)
+            {
+                gooflingCharge -= Time.fixedDeltaTime * .9f;
+            }
+            gooflingCharge = Mathf.Clamp(gooflingCharge,0,1);
         }
-        joint.axis = tangentVector;//spline.transform.TransformPoint(tangentVector);
         
-        axi = joint.axis;
-        anch = joint.connectedAnchor;
-        //transform.rotation = Quaternion.LookRotation(tangentVector);
-        Debug.DrawRay(transform.position,playerVel,Color.red);
-        Debug.DrawRay(transform.position,tangentVector,Color.green);
-
+        debugTime = time;
+        if(time < 0 || time > 1)
+        {
+            FindNewRailCast();
+            DisableJoint();
+            
+            //disconnectng from the rail should be its own function
+            if(OnRail)
+            {
+                transform.position += model.localPosition;
+                model.localPosition = Vector3.zero;
+                
+                OnRail = false;
+            }
+            jumpRegroundCooldown = .1f;
+        }
+        //the force for playermovement
+        rb.AddForce(VelocityChange * 10,ForceMode.Force);
     }
-    private Vector3 anch = Vector3.zero;
-    private Vector3 axi = Vector3.zero;
-    private void OnDrawGizmos() {
-        Gizmos.DrawSphere(anch,.5f);
-        Debug.DrawRay(anch,axi,Color.blue);
+
+    private void FindNewRailCast()
+    {
+        if(Physics.SphereCast(transform.position,.5f,Vector3.down,out RaycastHit hit))
+        {
+            if(hit.collider.tag == "Rail" && hit.collider != splineCollider)
+            {
+                spline = hit.collider.GetComponent<SplineContainer>();
+                splineCollider.enabled = true;
+                splineCollider = hit.collider;
+                Debug.Log("found a new rail, baby!");
+            }
+        }
+    }
+    private int ticksWithoutRail = 0;
+    private void LateUpdate() {
+        if(fakeObject && !OnRail)
+            transform.position = new Vector3(fakeObject.transform.position.x,transform.position.y,fakeObject.transform.position.z);
+        if(fakeObject)
+        if(Vector3.Distance(fakeJoint.connectedAnchor, fakeObject.transform.position) > 3)
+        {
+            var lastSpline = spline;
+            FindNewRailCast();
+            if(spline == lastSpline)
+                ticksWithoutRail++;
+            else
+                ticksWithoutRail = 0;
+        }
+        else if(Vector3.Distance(joint.connectedAnchor,transform.position) > 3)
+        {
+            var lastSpline = spline;
+            FindNewRailCast();
+            if(spline == lastSpline)
+                ticksWithoutRail++;
+            else
+                ticksWithoutRail = 0;
+        }
+        else
+            ticksWithoutRail = 0;
+           
+        //fully disable the joint when disconnected for 3 ticks
+        if(ticksWithoutRail >3)
+            DisableJoint();
     }
 
+    private void OnDrawGizmos()
+    {
+        if(fakeObject)
+            Gizmos.DrawSphere(fakeObject.transform.position,.5f);
+    }
+
+    void HandleJump(Vector3 direction, float force)
+    {
+        PartiallyDisableJoint();
+        rb.AddForce(direction.normalized * (force), ForceMode.Impulse);
+        transform.position += direction.normalized * .5f;
+        model.transform.localPosition = Vector3.zero;
+        OnRail = false;
+        
+    }
     private float textPos = 0;
     private void DrawText(string text)
     {
@@ -214,13 +357,22 @@ public class Player : MonoBehaviour
     {
         textPos = 0;
         DrawText(OnRail?"ONRAIL":"CRINGEFAIL");
-        DrawText(anch.ToString());
         DrawText("Rail: " + spline.name);
+        DrawText(BelowRail.ToString());
+        DrawText("Goofling Charge: " + gooflingCharge);
+        DrawText("Goofling Charge Reset: " + gooflingResetTimer);
+        DrawText("JumpCooldown: " + jumpRegroundCooldown);
+        DrawText("Inputs: " + inputVector);
+        DrawText("TicksWithoutRail: " + ticksWithoutRail);
+        DrawText("Time: " + debugTime);
     }
 
-    private void OnCollisionEnter(Collision other) {
+
+    private void CheckCollider(Collision other)
+    {
         //reattatch the player to the rail
         //also set the spline to the new spline object
+        if(jumpRegroundCooldown <= 0)
         if(other.collider.tag == "Rail")
         {
             spline = other.gameObject.GetComponent<SplineContainer>();
@@ -238,6 +390,12 @@ public class Player : MonoBehaviour
                 Debug.LogError("WARNING! Object tagged 'Rail' needs to have a SplineContainer! If you see this error, either add a spline to the object OR contact Mars and ask if they can add a feature!");
             }
         }
+    }
+    private void OnCollisionEnter(Collision other) {
+        CheckCollider(other);
+    }
+    private void OnCollisionStay(Collision other) {
+        CheckCollider(other);
     }
 }
 
