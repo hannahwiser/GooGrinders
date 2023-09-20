33c34b0014e185e0e4caf1fccb9c7e922066d572
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-09-20 02:55:32 -0400

    Added a death mechanic.

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,45 @@
+/**
+ * File: PlayerLife
+ * Programmer: Sagar Patel
+ * Description: Player movement script made using this video guide: https://www.youtube.com/watch?v=YQEq6Lkd69c
+ * Date: Sept 19, 2023
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    bool dead = false;
+
+    private void Update()
+    {
+        if (transform.position.y < -7.5f && !dead) {
+            Die();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.gameObject.CompareTag("Enemy Body")) {
+        // GetComponent<MeshRenderer>().enabled = false;
+        // GetComponent<Rigidbody>().isKinematic = true;
+        // GetComponent<PlayerMovement>().enabled = false;
+        //    Die();
+        //}
+    }
+
+    void Die()
+    {
+        Invoke(nameof(ReloadLevel), 1.3f);
+        dead = true;
+        Debug.Log("The player has died.");
+    }
+
+    void ReloadLevel() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
