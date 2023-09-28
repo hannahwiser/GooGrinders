0512300a4c91e021e89a067fee3abd8a89f11ca4
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-09-28 10:11:55 -0400

    Added a horizontal speed limit control

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,9 @@ using UnityEngine.Splines;
 using Unity.Mathematics;
 public class Player : MonoBehaviour
 {
+    //Speed limit
+    public float horizontalSpeedLimit = 30.0f;
+
     // stored objects
     private SplineContainer spline;
     private Collider splineCollider;
@@ -159,6 +162,14 @@ public class Player : MonoBehaviour
 
 
         MainState();
+
+        // Limit horizontal movement speed
+        Vector3 horizontalVelocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        if (horizontalVelocity.magnitude > horizontalSpeedLimit)
+        {
+            Vector3 clampedVelocity = horizontalVelocity.normalized * horizontalSpeedLimit;
+            rb.velocity = new Vector3(clampedVelocity.x, rb.velocity.y, clampedVelocity.z);
+        }
     }
 
     private float debugTime = 0;
