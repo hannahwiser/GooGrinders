3759c753488c36d9889226702a35763f806df419
hannahwiser <34260822+hannahwiser@users.noreply.github.com>
2023-09-28 20:21:15 -0400

    Click to interact
    Box textures, raycasts blocked appropriately, better intro and zoom in on box instructions

---
+++ Assets/Scripts/UI Scripts/Main Menu/BlockRaycasts.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//hannah's script
+public class BlockRaycasts : MonoBehaviour
+{
+    public GameObject[] rayObjects;
+
+    void Start()
+    {
+        rayObjects = GameObject.FindGameObjectsWithTag("Raycast Target");
+        BlockRaycast();
+    }
+
+    public void BlockRaycast()
+    {
+        for (int i = 0; i < rayObjects.Length; i++)
+        {
+            Debug.Log("run");
+            rayObjects[i].GetComponent<Collider>().enabled = false;
+        }
+    }
+
+    public void UnBlockRaycast()
+    {
+        for (int i = 0; i < rayObjects.Length; i++)
+        {
+            rayObjects[i].GetComponent<Collider>().enabled = true;
+        }
+    }
+}
--- Assets/Scripts/UI Scripts/Main Menu/MainMenuBehavior.cs
+++ Assets/Scripts/UI Scripts/Main Menu/MainMenuBehavior.cs
@@ -1,42 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
+//Hannah's script
+//This is attached to two objects in the Main Menu scene
+//The objects are 'Lid' and 'Flask Export v2'
 public class MainMenuBehavior : MonoBehaviour
 {
     [SerializeField] private Animator anim;
     private bool isZoomed = false;
     public Button startButton;
+    public string zoomInTitle;
+    public string zoomOutTitle;
+    public GameObject textObject;
+    public GameObject raycastBlock;
 
     void Start()
     {
         startButton.enabled = false;
+        textObject.SetActive(false);
+        raycastBlock.SetActive(false);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape) && isZoomed) //makes sure we are not already zoomed in when player presses esc
         {
-            anim.Play("FlaskDown");
+            textObject.GetComponent<TextMeshProUGUI>().text = "click to interact";
+            anim.Play(zoomOutTitle);
             StartCoroutine(WaitForAnim(false));
         }
     }
 
+    void OnMouseOver()
+    {
+        textObject.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        textObject.SetActive(false);
+    }
+
     void OnMouseDown() // flask is clicked
     {
-        if (!isZoomed)
+        if (!isZoomed) //if not already zoomed only
         {
-            //start async loading the game scene here for ease of transfer
-            anim.Play("FlaskUp");
+            textObject.GetComponent<TextMeshProUGUI>().text = "";
+            anim.Play(zoomInTitle);
             StartCoroutine(WaitForAnim(true));
         }
     }
 
     IEnumerator WaitForAnim(bool target) //coroutine to make sure player cannot double input.. not necessary but hunter requested lol
     {
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(.02f);
+        raycastBlock.SetActive(target);
         isZoomed = target;
         startButton.enabled = target;
     }
