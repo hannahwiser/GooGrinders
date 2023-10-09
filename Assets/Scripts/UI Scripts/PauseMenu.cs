72c78dc2f4bb34178f1074c8f12413dc08e0b912
hannahwiser <34260822+hannahwiser@users.noreply.github.com>
2023-10-09 19:55:40 -0400

    Pause Menu Works
    Pause menu works fully

--- Assets/Scripts/UI Scripts/PauseMenu.cs
+++ Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -41,5 +42,9 @@ public class PauseMenu : MonoBehaviour
         anim.Play("PauseFadeOut");
     }
 
-    
+    public void ToScene(int scene)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(scene);
+    }
 }
