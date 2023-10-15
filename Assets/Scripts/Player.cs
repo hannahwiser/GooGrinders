14938b1d022edb72d71aa8924c7dfba4aa874574
hannahwiser <34260822+hannahwiser@users.noreply.github.com>
2023-10-15 16:58:49 -0400

    See how this feels
    Modified vector 3 numbers

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -232,7 +232,8 @@ public class Player : MonoBehaviour
             jump.time = .1f;
             jump.Play();
             splineCollider.enabled = false;
-            HandleJump(jumpUpVector, Mathf.Clamp(gooflingMultiplier * gooflingCharge, 3, 6));
+            //HandleJump(jumpUpVector, Mathf.Clamp(gooflingMultiplier * gooflingCharge, 3, 6));
+            HandleJump(new Vector3(2,5,0), Mathf.Clamp(gooflingMultiplier * gooflingCharge, 3, 6));
             //HandleJump(Vector3.up,1);
             tempFlingParticle.Play();
         }
