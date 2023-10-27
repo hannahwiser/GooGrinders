15d9db165e139cd7c97bfc730ca7adaa79df193d
epicjakman <epicjakman@gmail.com>
2023-10-26 20:31:27 -0400

    Enemy anims and rail switch sounds

--- Assets/Scripts/PlayerAnimController.cs
+++ Assets/Scripts/PlayerAnimController.cs
@@ -6,6 +6,7 @@ public class PlayerAnimController : MonoBehaviour
 {
     public Player player;
     public Animator animator;
+    public AudioSource railSwitchSFX;
     private bool prevBelowRail = false;
     private bool isCharging = false;
     private float chargeStartTime = 0.0f;
@@ -13,47 +14,51 @@ public class PlayerAnimController : MonoBehaviour
     void Update()
     {
         animator.SetBool("OnRail", player.OnRail);
-        if (player.BelowRail != prevBelowRail)
-        {
-            if (prevBelowRail)
-                animator.Play("BottomToTop");
-            else
-                animator.Play("TopToBottom");
-        }
-
-        // check if the player started charging the jump (this is the default state, because we don't
-        // actually know if they're jumping immediately or charging the jump yet)
-        if (player.jumpInput && !isCharging)
-        {
-            chargeStartTime = Time.time;
-            isCharging = true;
-        }
-
-        // check if the player released the jump button or if they've exceeded 0.2 seconds of charge which means
-        // that they're definitely trying to charge the jump meter
-        if (!player.jumpInput || (Time.time - chargeStartTime) >= 0.2f)
-        {
-            isCharging = false;
-        }
-
-        // play the appropriate jump or charging animation
-        if (isCharging)
-        {
-            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        if(player.OnRail){
+            if (player.BelowRail != prevBelowRail)
             {
-                // Only initialize ChargingJump once and play until the end
-                animator.Play("ChargingJump", 0, 0);
+                if (prevBelowRail)
+                    animator.Play("BottomToTop");
+                else
+                    animator.Play("TopToBottom");
+                if(railSwitchSFX)
+                    railSwitchSFX.PlayOneShot(railSwitchSFX.clip);
             }
-            else if (!animator.GetCurrentAnimatorStateInfo(0).IsName("ChargingJump"))
+
+            // check if the player started charging the jump (this is the default state, because we don't
+            // actually know if they're jumping immediately or charging the jump yet)
+            if (player.jumpInput && !isCharging)
             {
-                animator.Play("ChargingJump");
+                chargeStartTime = Time.time;
+                isCharging = true;
             }
+
+            // check if the player released the jump button or if they've exceeded 0.2 seconds of charge which means
+            // that they're definitely trying to charge the jump meter
+            if (!player.jumpInput || (Time.time - chargeStartTime) >= 0.2f)
+            {
+                isCharging = false;
+            }
+
+            // play the appropriate jump or charging animation
+            if (isCharging)
+            {
+                if (animator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+                {
+                    // Only initialize ChargingJump once and play until the end
+                    animator.Play("ChargingJump", 0, 0);
+                }
+                else if (!animator.GetCurrentAnimatorStateInfo(0).IsName("ChargingJump"))
+                {
+                    animator.Play("ChargingJump");
+                }
+            }
+            
         }
-        else if (player.jumpInputRelease)
+        if (player.jumpInputRelease)
         {
             animator.Play("Jump");
         }
-
         animator.SetFloat("MovementInput", player.inputVector.x + .5f);
         prevBelowRail = player.BelowRail;
     }
