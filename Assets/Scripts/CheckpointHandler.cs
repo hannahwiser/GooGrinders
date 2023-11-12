abe0a1e7568f5c4477f4b78618069e276ea48c85
hannahwiser <34260822+hannahwiser@users.noreply.github.com>
2023-11-12 18:44:20 -0500

    DeathPopup

--- Assets/Scripts/CheckpointHandler.cs
+++ Assets/Scripts/CheckpointHandler.cs
@@ -1,20 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CheckpointHandler : MonoBehaviour
 {
     public string[] FunnyTexts;
-    
+    public TextMeshProUGUI currentText, balanceText, promptText, confirmSpendText;
+    public Animator anim;
+    public GameObject deathCanvas;
+    public int gorgerCost;
+
+    private void Awake()
+    {
+        deathCanvas.SetActive(false);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+
+    public void DeathPopup()
+    {
+        Time.timeScale = 0;
+
+        promptText.SetText("Would you like to spend " + gorgerCost + " googorger points to continue?");
+        currentText.SetText(FunnyTexts[Random.Range(0, FunnyTexts.Length-1)]);
+        balanceText.SetText("current balance: " + PlayerPrefs.GetInt("PlayerScore") + " points");
+
+        if (PlayerPrefs.GetInt("PlayerScore") < gorgerCost)
+        {
+            confirmSpendText.SetText("Not Enough Points");
+        }
+        else
+        {
+            confirmSpendText.SetText("Spend Points");
+        }
+
+        deathCanvas.SetActive(true);
+    }
+
 }
