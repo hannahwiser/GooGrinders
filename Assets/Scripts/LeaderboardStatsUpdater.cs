5478a04311a16cf1e00a94e9f2b8698c3175301f
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-16 15:27:49 -0500

    Added a conversion from seconds to hours:minutes:seconds

--- Assets/Scripts/LeaderboardStatsUpdater.cs
+++ Assets/Scripts/LeaderboardStatsUpdater.cs
@@ -15,7 +15,16 @@ public class LeaderboardStatsUpdater : MonoBehaviour
     {
         finalScoreText.text = finalScore.ToString();
         googorgersPoppedText.text = googorgersPopped.ToString();
-        totalTimeGrindedText.text = totalTimeGrinded.ToString("F2") + "s";
+        totalTimeGrindedText.text = ConvertToTimeFormat(totalTimeGrinded);
         totalAttemptsText.text = totalAttempts.ToString();
+    }
+
+    private string ConvertToTimeFormat(float totalTime)
+    {
+        int hours = (int)(totalTime / 3600);
+        int minutes = (int)((totalTime % 3600) / 60);
+        int seconds = (int)(totalTime % 60);
+
+        return string.Format("{0:D2}:{1:D2}:{2:D2}", hours, minutes, seconds);
     }
 }
