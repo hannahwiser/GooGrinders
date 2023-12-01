e1f9a24f2727983eb0d0d59c5a1ac746d7022e13
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-11-30 23:04:31 -0500

    winner bug

--- Assets/Scripts/WinZone.cs
+++ Assets/Scripts/WinZone.cs
@@ -6,13 +6,15 @@ public class WinZone : MonoBehaviour
 {
     public CheckpointHandler chkptScript;
     public LeaderboardSequence leaderboardSequence;
+    public GameObject sporet;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            leaderboardSequence.SetWin(true);
+            //leaderboardSequence.SetWin(true);
             chkptScript.LetMeWin();
+            sporet.SetActive(false);
         }
     }
 }
